8fd0d5c0c2d402fa317df787592fa3664e6f55ef
Zuhaib khalifa <zuhaibkhalifa@gmail.com>
2022-06-16 15:07:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+// componentes are added here
+import Player from "./components/player";
+import Song from "./components/songs";
+//Styles components
+import "./styles/app.scss";
+
+//utils
+import data from "./util";
+
+function App() {
+  //State
+  const [Songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(Songs[1]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/util.js
@@ -0,0 +1,127 @@
+import { v4 as uuidv4 } from "uuid";
+function spotify() {
+  return [
+    {
+      name: "Blessed",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32860",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Stay Positive",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32859",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Curosoa",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32846",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflections",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32854",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Zephyr",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32865",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Jellyfish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32850",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Dialde up",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32883",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Serenity",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32843",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "In between",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32845",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32842",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "2Girls 1 Rose",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32890",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Morning View",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/Explore-Chillville-SUmmer.jpeg",
+      artist: "Moods, Yasper",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=32839",
+      color: ["#65B7DC", "EC79AD"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default spotify;
